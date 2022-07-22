02328c929c86e6ddd9c9dd7c4a590b0bab02ba1d
hadamelino <hadamuhammad@gmail.com>
2022-07-22 15:37:32 +0700

    setup file and core data object

---
+++ AcademyLOTracker/Progress/ViewModel/ProgressViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  ProgressViewModel.swift
+//  AcademyLOTracker
+//
+//  Created by Hada Melino Muhammad on 22/07/22.
+//
+
+import Foundation
+
+class ProgressViewModel {
+    
+    func fetchAcademyProgress() {
+        
+    }
+    
+    func fetchHighPriorityLO() {
+        
+    }
+    
+    func updateLearningProgress() {
+        
+    }
+    
+    
+}
