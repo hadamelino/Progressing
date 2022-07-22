b6f3aafe26641f50ea556c5efa5f06eaecda3d86
hadamelino <hadamuhammad@gmail.com>
2022-07-22 14:23:38 +0700

    Initial Commit

---
+++ AcademyLOTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AcademyLOTracker
+//
+//  Created by Hada Melino Muhammad on 22/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
